fba4600d5e4f437e44243b263e22a293951f227e
Robert Walecki <rw2614@imperial.ac.uk>
2017-02-07 01:48:25 +0000

    add test: run_pipeline

---
+++ tests/test_run_pipeline.py
@@ -0,0 +1,31 @@
+from skimage.io import imread
+import emotion_data
+path_to_shape_model = '/homes/rw2614/projects/shape_model/shape_predictor_68_face_landmarks.dat'
+
+img = imread('./test_data/images/test_01.jpg')
+print(img.shape)
+
+fg = emotion_data.provider.Facial_Expressions(
+    histogram_normalization = True,
+    mean_std_normalization = True,
+    make_grayscale = True,
+    path_to_shape_model = path_to_shape_model,
+    output_size = [128,192],
+    face_size = 128,
+    rotation_range = 90,
+    width_shift_range = 0.05,
+    height_shift_range = 0.05,
+    zoom_range = 0.05,
+    )
+
+out, pts = fg.run_pipeline(img, extract_bbox=False, preprocessing=False, augment=False)
+print(out.shape, out.max(), out.min())
+
+out, pts = fg.run_pipeline(img, extract_bbox=True,  preprocessing=False, augment=False)
+print(out.shape, out.max(), out.min())
+
+out, pts = fg.run_pipeline(img, extract_bbox=True,  preprocessing=True,  augment=False)
+print(out.shape, out.max(), out.min())
+
+out, pts = fg.run_pipeline(img, extract_bbox=True,  preprocessing=True,  augment=True)
+print(out.shape, out.max(), out.min())
