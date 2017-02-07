08a415c9f1c681944fdf949175d2b1c6d8370a13
Robert Walecki <rw2614@imperial.ac.uk>
2017-02-07 19:19:30 +0000

    rename project from emotion_data

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="emotion_data",
+    name="EmoData",
     version="0.0.1",
     author="Robert Walecki",
     author_email="r.walecki14@imperial.ac.uk",
